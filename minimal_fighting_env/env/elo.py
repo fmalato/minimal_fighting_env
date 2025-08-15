387319c9204c38207f2e3bd64d5a5e1c9a2eeb85
Mikko Turunen <59641298+miosturu@users.noreply.github.com>
2025-08-15 09:21:38 +0300

    Update elo.py
    Added comments for how the elo.py works

--- minimal_fighting_env/env/elo.py
+++ minimal_fighting_env/env/elo.py
@@ -1,5 +1,7 @@
 import math
 
+# Class to calculate the Elo-rankings
+# K is the maximum change of rank after win or loss
 class eloCalculator:
     k = 0
     def __init__(self, k=40):
@@ -19,9 +21,11 @@ class eloCalculator:
 
         return [p1Elo + self.k * (p1Win - p1), p2Elo + self.k * (p2Win - p2)]
 
+# Calculate the propability of winning from the ranks
 def p(a, b):
     return (1.0 / (1.0 + math.pow(10.0, (b - a) / 400)))
 
+# Main function used for testing
 if __name__ == "__main__":
     calc = eloCalculator()
     print(calc.calculateRankChange(1500, 1500, 0))
